48e86361aa86570427175fd7a30c67460c938b74
Aldiantoro Nugroho <kriwil@gmail.com>
2014-04-15 12:37:37 +0800

    Add CourseProxy to admin, and exclude created_at and modified_at.

--- labster/admin.py
+++ labster/admin.py
@@ -1,14 +1,24 @@
+from django.conf import settings
 from django.contrib import admin
 
-from labster.models import LanguageLab, Lab, QuizBlockLab
+from labster.models import LanguageLab, Lab, QuizBlockLab, CourseProxy
 from labster.forms import LabAdminForm
 
 
-class LabAdmin(admin.ModelAdmin):
+class BaseAdmin(admin.ModelAdmin):
+    exclude = ('created_at', 'modified_at')
+
+
+class LabAdmin(BaseAdmin):
     list_display = ('name', 'description', 'url', 'wiki_url', 'screenshot')
     form = LabAdminForm
 
 
+class CourseProxyAdmin(BaseAdmin):
+    list_display = ('lab', 'course_id')
+
+
 admin.site.register(LanguageLab)
 admin.site.register(Lab, LabAdmin)
-admin.site.register(QuizBlockLab)
+admin.site.register(QuizBlockLab, BaseAdmin)
+admin.site.register(CourseProxy, CourseProxyAdmin)

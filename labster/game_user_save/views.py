3238304921440fe0dcc1ba623a7232bf9fa38f26
Aslam <aslamhadi@gmail.com>
2014-04-24 17:14:43 +0800

    refactor view game_user_save

--- labster/game_user_save/views.py
+++ labster/game_user_save/views.py
@@ -3,8 +3,6 @@ import json
 from django.forms import ModelForm
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.db.models import Q
-from django.core.exceptions import ObjectDoesNotExist
 
 from labster.models import GameUserSave
 
@@ -26,13 +24,11 @@ def game_user_save_post(request):
     user_id = request.POST.get('user')
     lab_id = request.POST.get('lab')
     try:
-        game_user_save = GameUserSave.objects.get(
-            Q(user=user_id),
-            Q(lab=lab_id)
-        )
-        form = GameUserSaveForm(request.POST, request.FILES, instance=game_user_save)
-    except ObjectDoesNotExist:
-        form = GameUserSaveForm(request.POST, request.FILES)
+        game_user_save = GameUserSave.objects.get(user=user_id, lab=lab_id)
+    except GameUserSave.DoesNotExist:
+        game_user_save = None
+
+    form = GameUserSaveForm(request.POST, request.FILES, instance=game_user_save)
 
     if form.is_valid():
         form.save()

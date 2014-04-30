4725da1db15b7521270f7b8a6084a669277a5f4b
Aldiantoro Nugroho <kriwil@gmail.com>
2014-04-30 12:21:46 +0800

    Bump version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='labster',
-    version='0.1',
+    version='0.1.1',
     packages=['labster'],
     include_package_data=True,
     license='BSD License',  # example license

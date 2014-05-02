3935ee30fcf5ac2085b3c516f3ebecb54c1b994d
Aldiantoro Nugroho <kriwil@gmail.com>
2014-05-02 14:39:44 +0800

    Fix admin error, wrong models.

--- labster/admin.py
+++ labster/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from labster.models import LanguageLab, Lab, QuizBlockLab, LabProxy,\
-    GameErrorInfo, UserDeviceInfo, UserSave
+    ErrorInfo, DeviceInfo, UserSave
 from labster.forms import LabAdminForm
 
 
@@ -32,18 +32,26 @@ class UserSaveAdmin(BaseAdmin):
         return obj.lab_proxy.lab.name
 
 
-class GameErrorInfoAdmin(admin.ModelAdmin):
+class ErrorInfoAdmin(admin.ModelAdmin):
     list_display = ('user', 'lab', 'browser', 'os', 'message', 'date_encountered')
 
+    def lab(self, obj):
+        return obj.lab_proxy.lab.name
 
-class UserDeviceInfoAdmin(admin.ModelAdmin):
-    list_display = ('user', 'lab', 'device_id', 'frame_rate', 'type', 'os', 'ram', 'processor', 'cores', 'gpu', 'memory', 'fill_rate', 'shader_level', 'quality', 'misc')
+
+class DeviceInfoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'lab', 'device_id', 'frame_rate', 'type', 'os',
+                    'ram', 'processor', 'cores', 'gpu', 'memory', 'fill_rate',
+                    'shader_level', 'quality', 'misc')
+
+    def lab(self, obj):
+        return obj.lab_proxy.lab.name
 
 
 admin.site.register(LanguageLab)
 admin.site.register(Lab, LabAdmin)
 admin.site.register(QuizBlockLab, BaseAdmin)
 admin.site.register(LabProxy, LabProxyAdmin)
-admin.site.register(GameErrorInfo, GameErrorInfoAdmin)
-admin.site.register(UserDeviceInfo, UserDeviceInfoAdmin)
+admin.site.register(ErrorInfo, ErrorInfoAdmin)
+admin.site.register(DeviceInfo, DeviceInfoAdmin)
 admin.site.register(UserSave, UserSaveAdmin)

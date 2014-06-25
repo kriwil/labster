c98b23540ab7e6dd6f039323a41e20bad4d4f9b8
Aldiantoro Nugroho <kriwil@gmail.com>
2014-06-25 15:19:52 +0800

    Add is_active field to LabProxy.

--- labster/models.py
+++ labster/models.py
@@ -126,7 +126,8 @@ class QuizBlockLab(models.Model):
 class LabProxy(models.Model):
 
     lab = models.ForeignKey(Lab)
-    location_id = models.CharField(max_length=100)
+    location_id = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
 
     created_at = models.DateTimeField(default=timezone.now)
     modified_at = models.DateTimeField(default=timezone.now)

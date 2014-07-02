b73be7210a7c04545b092852412f7555ce0333fc
Aldiantoro Nugroho <kriwil@gmail.com>
2014-07-02 12:04:15 +0800

    Removed unused files.

--- labster/tests/__init__.py
+++ labster/tests/__init__.py
@@ -1,6 +1,1 @@
-from .game_error_info.test_views import *  # noqa
-from .game_user_save.test_views import *  # noqa
-from .lab_proxies.test_views import *  # noqa
-from .test_forms import *  # noqa
 from .test_utils import *  # noqa
-from .user_device_info.test_views import *  # noqa

465ae1a2875573d07f1839943775f4965da2b2b5
Aldiantoro Nugroho <kriwil@gmail.com>
2014-07-14 17:11:47 +0700

    Remove unused views and urls

--- labster/api/views.py
+++ labster/api/views.py
@@ -6,8 +6,7 @@ from django.http import QueryDict
 from django.shortcuts import get_object_or_404
 
 from rest_framework import status
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, ListCreateAPIView
-from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -15,9 +14,8 @@ from courseware.module_render import _invoke_xblock_handler
 from opaque_keys.edx.keys import UsageKey
 from xmodule.modulestore.django import modulestore
 
-from labster.api.serializers import LabSerializer, LabProxySerializer, ProblemSerializer, UserSaveSerializer, ErrorInfoSerializer, DeviceInfoSerializer
-from labster.models import Lab, QuizBlock, Problem, LabProxy, UserSave, ErrorInfo, DeviceInfo
-from labster.models import create_lab_proxy, update_lab_proxy, UserLabProxy
+from labster.api.serializers import UserSaveSerializer, ErrorInfoSerializer, DeviceInfoSerializer
+from labster.models import UserSave, ErrorInfo, DeviceInfo
 
 
 def invoke_xblock_handler(*args, **kwargs):
@@ -25,107 +23,6 @@ def invoke_xblock_handler(*args, **kwargs):
     Wrapper so it could be mocked
     """
     return _invoke_xblock_handler(*args, **kwargs)
-
-
-class LabList(ListAPIView):
-    model = Lab
-    queryset = Lab.objects.all()
-    serializer_class = LabSerializer
-
-
-class LabDetail(RetrieveAPIView):
-    model = Lab
-    queryset = Lab.objects.all()
-    serializer_class = LabSerializer
-
-
-class QuizBlockList(ListAPIView):
-    model = QuizBlock
-    queryset = QuizBlock.objects.all()
-
-
-class QuizBlockDetail(RetrieveAPIView):
-    model = QuizBlock
-    queryset = QuizBlock.objects.all()
-
-
-class ProblemList(ListCreateAPIView):
-    model = Problem
-    queryset = Problem.objects.all()
-    serializer_class = ProblemSerializer
-
-
-class ProblemDetail(RetrieveUpdateDestroyAPIView):
-    model = Problem
-    queryset = Problem.objects.all()
-    serializer_class = ProblemSerializer
-
-
-class LabProxyList(ListCreateAPIView):
-    model = LabProxy
-    queryset = LabProxy.objects.all()
-    serializer_class = LabProxySerializer
-
-    def create(self, request, *args, **kwargs):
-        lab_id = request.DATA.get('lab')
-        location_id = request.DATA.get('location_id')
-        lab_proxy_id = request.DATA.get('lab_proxy')
-
-        if not all([lab_id, location_id]):
-            return Response({}, status=status.HTTP_400_BAD_REQUEST)
-
-        if lab_proxy_id:
-            lab_proxy = update_lab_proxy(lab_proxy_id, lab_id)
-        else:
-            lab_proxy = create_lab_proxy(lab_id, location_id)
-
-        serializer = LabProxySerializer(lab_proxy)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class LabProxyDetail(RetrieveAPIView):
-    model = LabProxy
-    queryset = LabProxy.objects.all()
-    serializer_class = LabProxySerializer
-
-
-class CreateUserLabProxy(APIView):
-
-    def get(self, request, *args, **kwargs):
-        user_id = request.GET.get('user')
-        lab_proxy_id = request.GET.get('lab_proxy')
-
-        user_lab_proxy = get_object_or_404(UserLabProxy, lab_proxy_id=lab_proxy_id, user_id=user_id)
-        response = {
-            'id': user_lab_proxy.id,
-            'completed': user_lab_proxy.completed,
-        }
-        return Response(response)
-
-    def post(self, request, *args, **kwargs):
-        data = request.DATA
-
-        user_id = data.get('user')
-        lab_proxy_id = data.get('lab_proxy')
-        completed = data.get('completed', False)
-
-        user = get_object_or_404(User, id=user_id)
-        lab_proxy = get_object_or_404(LabProxy, id=lab_proxy_id)
-
-        user_lab_proxy, created = UserLabProxy.objects.get_or_create(
-            user=user, lab_proxy=lab_proxy, defaults={'completed': completed})
-
-        if not user_lab_proxy.completed:
-            user_lab_proxy.completed = True
-            user_lab_proxy.save()
-
-        response = {
-            'user_lab_proxy': user_lab_proxy.id,
-            'completed': user_lab_proxy.completed,
-        }
-
-        http_status = status.HTTP_201_CREATED if created else status.HTTP_204_NO_CONTENT
-        return Response(response, status=http_status)
 
 
 class CreateUserSave(ListCreateAPIView):
--- labster/api_v2_urls.py
+++ labster/api_v2_urls.py
@@ -1,10 +1,5 @@
 from django.conf.urls import patterns, url
 
-from labster.api.views import LabList, LabDetail
-from labster.api.views import LabProxyList, LabProxyDetail
-from labster.api.views import ProblemList, ProblemDetail
-from labster.api.views import QuizBlockList, QuizBlockDetail
-from labster.api.views import CreateUserLabProxy
 from labster.api.views import CreateUserSave, CreateErrorInfo, CreateDeviceInfo
 from labster.api.views import CourseLab, AnswerProblem
 
@@ -14,21 +9,7 @@ urlpatterns = patterns('',  # nopep8
     url('^course-lab/(?P<location>[^\/]*)/$', CourseLab.as_view()),
     url('^course-lab/(?P<location>[^\/]*)/answer-problem/$', AnswerProblem.as_view()),
 
-    url('^labs/$', LabList.as_view(), name='lab-list'),
-    url('^labs/(?P<pk>\d+)/$', LabDetail.as_view(), name='lab-detail'),
-
-    url('^quizblocks/$', QuizBlockList.as_view(), name='quiz-block-list'),
-    url('^quizblocks/(?P<pk>\d+)/$', QuizBlockDetail.as_view(), name='quiz-block-detail'),
-
-    url('^problems/$', ProblemList.as_view(), name='problem-list'),
-    url('^problems/(?P<pk>\d+)/$', ProblemDetail.as_view(), name='problem-detail'),
-
-    url('^lab-proxies/$', LabProxyList.as_view(), name='lab-proxy-list'),
-    url('^lab-proxies/(?P<pk>\d+)/$', LabProxyDetail.as_view(), name='lab-proxy-detail'),
-
     url('^user-save/$', CreateUserSave.as_view(), name='user-save'),
     url('^error-info/$', CreateErrorInfo.as_view(), name='error-info'),
     url('^device-info/$', CreateDeviceInfo.as_view(), name='device-info'),
-
-    url('^user-lab-proxy/$', CreateUserLabProxy.as_view(), name='user-lab-proxy'),
 )

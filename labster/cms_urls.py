90b60472023e65efd55b90686f48019f75fcaa56
Aldiantoro Nugroho <kriwil@gmail.com>
2014-07-15 10:11:49 +0700

    Remove unused views

--- labster/cms_urls.py
+++ labster/cms_urls.py
@@ -3,13 +3,4 @@ from django.conf.urls import patterns, include, url  # noqa
 
 urlpatterns = patterns('labster.cms.views',  # nopep8
     url('^duplicate-lab/$', 'duplicate_lab', name='labster_duplicate_lab'),
-
-    url('^labs/$', 'lab_list'),
-    url('^labs/(?P<id>\d+)/$', 'lab_detail'),
-    url('^labs/(?P<lab_id>\d+)/create-quiz-block/$', 'create_quiz_block', name='labster_create_quiz_block'),
-
-    url('^lab-proxies/(?P<id>\d+)/$', 'lab_proxy_detail'),
-
-    url('^quiz-blocks/(?P<id>\d+)/$', 'quiz_block_detail', name='labster_quiz_block_detail'),
-    url('^quiz-blocks/(?P<quiz_block_id>\d+)/create-problem/$', 'create_problem', name='labster_create_problem'),
 )

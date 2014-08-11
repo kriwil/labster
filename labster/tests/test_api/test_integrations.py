1da31a8e2b9b3ef54283be2d14178988b71d2c87
Aldiantoro Nugroho <aldi@labster.com>
2014-08-11 17:22:31 +0800

    CreateUserSave test

--- labster/tests/test_api/test_integrations.py
+++ labster/tests/test_api/test_integrations.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.urlresolvers import reverse
+from django.test import TestCase
 
 from rest_framework.authtoken.models import Token
 
@@ -29,6 +30,17 @@ class NoGetMixin(object):
     def test_get_not_allowed(self):
         response = self.client.get(self.url, **self.headers)
         self.assertEqual(response.status_code, 405)
+
+
+class AuthGetOnlyMixin(object):
+
+    def test_get_not_found(self):
+        response = self.client.get(self.url, **self.headers)
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_not_authenticated(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 401)
 
 
 class AuthPostOnlyMixin(object):
@@ -193,3 +205,55 @@ class CreateDeviceInfoTest(NoGetMixin, AuthPostOnlyMixin, TestCase):
         device_info = device_infos[0]
         for key, value in post_data.items():
             self.assertEqual(getattr(device_info, key), value)
+
+
+class CreateUserSaveTest(AuthGetOnlyMixin, AuthPostOnlyMixin, TestCase):
+
+    def setUp(self):
+        self.lab = LabFactory()
+        self.user = UserFactory()
+        self.lab_proxy = create_lab_proxy(lab=self.lab)
+        self.user_save_file = SimpleUploadedFile("file.txt", "this is the content")
+
+        self.url = reverse('labster-api-v2:user-save', args=[self.lab_proxy.location])
+        self.headers = get_auth_header(self.user)
+
+    def test_get_found(self):
+        UserSaveFactory(user=self.user, lab_proxy=self.lab_proxy,
+                        save_file=self.user_save_file)
+        response = self.client.get(self.url, **self.headers)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_created_without_initial_lab_proxy(self):
+        lab_proxy_location = 'somerandomtext'
+        self.url = reverse('labster-api-v2:user-save', args=[lab_proxy_location])
+        post_data = {
+            'save_file': self.user_save_file,
+        }
+        response = self.client.post(self.url, post_data, **self.headers)
+        self.assertEqual(response.status_code, 201)
+
+        user_saves = UserSave.objects.filter(
+            user=self.user, lab_proxy__location=lab_proxy_location)
+        self.assertTrue(user_saves.exists())
+
+        user_save = user_saves[0]
+        self.assertIsNotNone(user_save.save_file)
+
+    def test_post_exists(self):
+        user_save = UserSaveFactory(
+            user=self.user, lab_proxy=self.lab_proxy,
+            save_file=self.user_save_file)
+
+        post_data = {
+            'save_file': self.user_save_file,
+        }
+
+        # whenever we post another user save, it will replace the old data
+        response = self.client.post(self.url, post_data, **self.headers)
+        self.assertEqual(response.status_code, 204)
+
+        user_saves = UserSave.objects.filter(user=self.user, lab_proxy=self.lab_proxy)
+        self.assertEqual(user_saves.count(), 1)
+
+        self.assertEqual(user_saves[0].id, user_save.id)

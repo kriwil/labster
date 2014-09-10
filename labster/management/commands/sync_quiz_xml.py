46f29be6f38143a6e1bc022f5f3b299c6ade7b66
Aldiantoro Nugroho <aldi@labster.com>
2014-09-10 14:10:12 +0700

    Update the sync_quiz_xml management to allow to sync all lab proxies.

--- labster/management/commands/sync_quiz_xml.py
+++ labster/management/commands/sync_quiz_xml.py
@@ -5,7 +5,11 @@ from courseware.courses import get_course_by_id
 from opaque_keys.edx.locations import SlashSeparatedCourseKey
 
 from labster.constants import ADMIN_USER_ID
+from labster.models import LabProxy
 from labster.quiz_blocks import sync_quiz_xml
+
+from opaque_keys.edx.keys import UsageKey
+from xmodule.modulestore.django import modulestore
 
 
 class Command(BaseCommand):
@@ -18,21 +22,45 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         user = User.objects.get(id=ADMIN_USER_ID)
-        course_id = args[0]
-        try:
-            section_name = args[1]
-        except IndexError:
-            section_name = ""
-        try:
-            sub_section_name = args[2]
-        except IndexError:
-            sub_section_name = ""
+        if args[0] == 'all':
+            self.stdout.write('updating all quiz xml\n')
 
-        org, number, run = course_id.split('/')
+            lab_proxies = LabProxy.objects.all()
+            for lab_proxy in lab_proxies:
+                self.stdout.write('... {} - {} - {}\n'.format(
+                    lab_proxy.id, lab_proxy.location, lab_proxy.lab.name))
 
-        course_key = SlashSeparatedCourseKey(org, number, run)
-        course = get_course_by_id(course_key)
+                locator = UsageKey.from_string(lab_proxy.location)
+                descriptor = modulestore().get_item(locator)
+                course_key = descriptor.location.course_key
+                course = get_course_by_id(course_key)
 
-        sync_quiz_xml(course, user, command=self,
-                      section_name=section_name,
-                      sub_section_name=sub_section_name)
+                for section in course.get_children():
+                    for sub_section in section.get_children():
+                        if not sub_section.lab_id:
+                            continue
+
+                        sync_quiz_xml(
+                            course, user, command=self,
+                            section_name=section.display_name,
+                            sub_section_name=sub_section.display_name)
+
+        else:
+            course_id = args[0]
+            try:
+                section_name = args[1]
+            except IndexError:
+                section_name = ""
+            try:
+                sub_section_name = args[2]
+            except IndexError:
+                sub_section_name = ""
+
+            org, number, run = course_id.split('/')
+
+            course_key = SlashSeparatedCourseKey(org, number, run)
+            course = get_course_by_id(course_key)
+
+            sync_quiz_xml(course, user, command=self,
+                        section_name=section_name,
+                        sub_section_name=sub_section_name)

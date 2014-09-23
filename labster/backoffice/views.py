8e7b29369507f6f5fa85afc83d0bceea71cc98c7
Aldiantoro Nugroho <aldi@labster.com>
2014-09-23 11:17:47 +0700

    Raise 404 if the user is not authorized to access backoffice.

--- labster/backoffice/views.py
+++ labster/backoffice/views.py
@@ -1,16 +1,17 @@
 import requests
 
 from django.contrib.auth.decorators import login_required
-from django.conf import settings
+from django.http import Http404
 
 from edxmako.shortcuts import render_to_response
 
 
-BASE_URL = settings.BACKOFFICE_BASE_URL
-LAB_LIST_URL = '{}/api/products/'.format(BASE_URL)
-
-
 def get_labs(format='json'):
+    from django.conf import settings
+
+    BASE_URL = settings.LABSTER_BACKOFFICE_BASE_URL
+    LAB_LIST_URL = '{}/api/products/'.format(BASE_URL)
+
     resp = requests.get(LAB_LIST_URL)
     assert resp.status_code == 200
 
@@ -21,6 +22,9 @@ def get_labs(format='json'):
 
 @login_required
 def home(request):
+    if not request.user.is_superuser and not request.user.is_staff:
+        raise Http404
+
     template_name = 'labster/backoffice.html'
     context = {
         'lab_list': get_labs(format='string'),
